4ccc2b6bb4bfc083823007905242665acc3ed2ba
wing <jesusblessf6@gmail.com>
2013-07-23 14:23:07 +0800

    sign in

--- app/controllers/sign_ins_controller.rb
+++ app/controllers/sign_ins_controller.rb
@@ -1,10 +1,22 @@
+#coding: utf-8  
+
 class SignInsController < ApplicationController
-  def new
-  end
+  	def new
+  	end
 
-  def create
-  end
+  	def create
+  		username = params[:sign_ins][:username]
+  		password = params[:sign_ins][:password]
+  		user = User.find_by_login_name(username)
+  		if user && user.authenticate(password) then
+  			session[:user_id] = user.id
+  			redirect_to default_path
+  		else
+  			flash.now[:error] = '用户名或密码错误'
+  			render 'new'
+  		end 
+  	end
 
-  def destroy
-  end
+  	def destroy
+  	end
 end
