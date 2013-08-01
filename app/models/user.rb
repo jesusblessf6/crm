414d1ff16d3858062ca6a690968f94bc74768038
jesusblessf6 <jesusblessf6@gmail.com>
2013-08-02 00:21:17 +0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :login_name, :name, :password, :password_confirmation
+  attr_accessible :login_name, :name, :password, :password_confirmation, :user_type
 
   has_secure_password
+  #user_type: 1 => admin
 end
